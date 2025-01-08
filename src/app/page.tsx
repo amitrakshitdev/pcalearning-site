a6ec5590f7b502445c3f3801e77d43cb39d0b4a5
amitrakshitdev <amit.rakshit008@gmail.com>
2025-01-08 21:14:29 +0530

    Content Change Marquee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,14 @@ import Marquee from "@/components/ui/Marquee/Marquee";
 import WhyPCALearning from "@/components/WhyPCALearning/WhyPCALearning";
 import clsx from "clsx";
 
+const marqueeText = "Even semester batches 2025 (B.Tech/BCA/MCA) going to be started from first week of February 2025."
+
 export default function Home() {
     return (
         <>
             <Carousel />
-            <Marquee duration={15} className={clsx(["bg-primary text-textSecondaryColor", "width-full", "py-2 px-4", "text-lg", "font-bold", "mb-1"])}>
-                <p>Unlock your coding potential! Admissions open now.</p>
+            <Marquee duration={20} className={clsx(["bg-primary text-textSecondaryColor", "width-full", "py-2 px-4", "text-lg", "font-bold", "mb-1"])}>
+                <p>{marqueeText}</p>
             </Marquee>
             <OurStory />
             <Courses />
