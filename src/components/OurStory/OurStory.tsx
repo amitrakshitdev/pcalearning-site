e86e7028f0e1cd1318533156e085b39a5c3f286f
amitrakshitdev <amit.rakshit008@gmail.com>
2025-03-05 19:21:26 +0530

    Content modifications as suggested by Arijit

--- src/components/OurStory/OurStory.tsx
+++ src/components/OurStory/OurStory.tsx
@@ -10,7 +10,7 @@ export default function OurStory() {
         <section className={clsx("OurStory relative w-full bg-customGreyColor p-4", ["md:p-10", "lg:p-20"])}>
             <div className={clsx(["flex flex-col", "bg-background", "rounded-md shadow-md overflow-hidden", "md:flex-row"])}>
                 <div className={clsx(["w-full", "text-textColor", "flex flex-col px-6 py-4", "md:w-2/3 md:px-12 md:py-10", "lg:px-32 lg:py-20"])}>
-                    <h1 className={clsx(["text-5xl font-bold text-center", "text-primary", "mb-4", "lg:text-7xl"])}>Our Story</h1>
+                    <h1 className={clsx(["text-5xl font-bold text-center underline", "text-primary", "mb-4", "lg:text-7xl"])}>Our Story</h1>
                     <h3 className={clsx(["font-semibold text-xl", "my-4"])}>Meet the Team Behind Your Coding Education</h3>
                     <p className={clsx(["text-sm font-light text-textColor", "text-gray-800", "lg:text-base"])}>
                         PCA Learning is a platform that offers a wide range of courses to help you learn coding and programming. We offer courses for beginners as well as advanced
